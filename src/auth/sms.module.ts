cc4d957b7f906dd945118169cffb36691edc213d
hirakroya50 <hirakroya50@gmail.com>
2026-06-22 21:51:15 +0530

    feat: add Fast2SMS to SMS provider factory

--- src/auth/sms.module.ts
+++ src/auth/sms.module.ts
@@ -3,11 +3,14 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SMS_PROVIDER } from './sms-provider';
 import { SmsMock } from './sms-mock';
 import { SmsTwilio } from './sms-twilio';
+import { SmsFast2Sms } from './sms-fast2sms';
 
 const SmsProviderFactory = {
   provide: SMS_PROVIDER,
   useFactory: (config: ConfigService) => {
-    return config.get('TWILIO_SID') ? new SmsTwilio(config) : new SmsMock();
+    if (config.get('FAST2SMS_API_KEY')) return new SmsFast2Sms(config);
+    if (config.get('TWILIO_SID')) return new SmsTwilio(config);
+    return new SmsMock();
   },
   inject: [ConfigService],
 };
